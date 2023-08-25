ad82bef95ce2242ac2b4e016740b77685489df8c
Sureshkumar-N <sureshsk7323@gmail.com>
2023-08-25 13:39:50 +0530

    f

--- src/List.js
+++ src/List.js
@@ -10,19 +10,39 @@ export default function List(){
             }
         );
     }
-    if(!tasks.length) {
-        return(
-            <h1>No task</h1>
-        );
+    const edit=(id) =>{
+        dispatch({
+            type:"EDIT",
+            payload:id
+        });
     }
     return(
         <div className="list">
-            <ul>
+            <table className="table">
+                <div>
+                    <tr>
+                        <th>No</th>
+                        <th>Task Name</th>
+                        <th>Status</th>
+                        <th>Edit</th>
+                        <th>Remove</th>
+                    </tr>
+                </div>
+                <div className="box"></div>
+                <div className="tr">
                 {
                     tasks.map((task,i)=>{
-                        return <li key={i}>{task} <button onClick={()=>delTask(i)}>DEL</button></li>
-                })}
-            </ul>
+                        return (<tr key={i}>
+                            <td>{i+1}</td>
+                            <td>{task.name}</td>
+                            <td>{task.status}</td>
+                            <td><button onClick={()=>{edit(i)}}>X</button></td>
+                            <td><button>Y</button></td>
+                        </tr>);
+                    }) 
+                }
+                </div>
+            </table>
         </div>
     );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,13 @@ import { createStore } from "redux";
 function reducer(state={tasks:[]}, action){
     switch (action.type) {
         case "ADD":
-            return {tasks:[...state.tasks,action.payload]};
+            return {tasks:[...state.tasks,{name:action.payload,status:"ToDo"}]};
         case "DEL":
             return {tasks:[...state.tasks.slice(0,action.payload),...state.tasks.slice(action.payload+1)]};
+        case "EDIT":
+            const updateTask=[...state.tasks];
+            updateTask[action.payload].status="In Progress";
+            return {tasks:updateTask};
         default:
             return state;
     }
